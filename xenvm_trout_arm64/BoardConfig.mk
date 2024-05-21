181f0efba8db23e1e9f18f7591c9915dafaf5a4e
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-05-21 11:14:25 +0000

    Fix SELinux issues found during CTS run
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Approved-by: Goncharuk Vladyslav <goncharuk_vladyslav@epam.com>

--- xenvm_trout_arm64/BoardConfig.mk
+++ xenvm_trout_arm64/BoardConfig.mk
@@ -3,3 +3,4 @@ BOARD_KERNEL_CMDLINE += androidboot.fstab_name=fstab androidboot.fstab_suffix=tr
 -include device/google/trout/trout_arm64/BoardConfig.mk
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648 # 2 GB
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy/private
