a55604b13ba4f97a3157b691b5fec93042f3f407
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-06-04 16:13:23 +0000

    Increase userdata to 7GB
    Increase userdata to 7GB to be able to accommodate
    all crash information during the CTS run.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Approved-by: Goncharuk Vladyslav <goncharuk_vladyslav@epam.com>

--- xenvm_trout_arm64/BoardConfig.mk
+++ xenvm_trout_arm64/BoardConfig.mk
@@ -1,6 +1,9 @@
 BOARD_KERNEL_CMDLINE += androidboot.fstab_name=fstab androidboot.fstab_suffix=trout_xenvm
 
+TARGET_USERDATAIMAGE_PARTITION_SIZE := 7516192768 # 7 GB
+
 -include device/google/trout/trout_arm64/BoardConfig.mk
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648 # 2 GB
+
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy/private
